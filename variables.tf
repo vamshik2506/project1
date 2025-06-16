e55b1db87359c9149196aa5a8fcfad22953f15c5
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-16 10:10:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,34 +1,57 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources in"
+  description = "AWS region"
   type        = string
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
   type        = string
 }
 
 variable "vpc_name" {
-  description = "Name tag for the VPC"
   type        = string
 }
 
 variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
   type        = string
 }
 
 variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet"
   type        = string
 }
 
 variable "public_subnet_az" {
-  description = "Availability zone for the public subnet"
   type        = string
 }
 
 variable "private_subnet_az" {
-  description = "Availability zone for the private subnet"
   type        = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
 }
