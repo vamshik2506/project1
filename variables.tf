c39cea08796bb49e89ec35a243ca08d16847632f
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-16 14:07:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,57 +1,11 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "vpc_cidr" {
-  type        = string
-}
-
-variable "vpc_name" {
-  type        = string
-}
-
-variable "public_subnet_cidr" {
-  type        = string
-}
-
-variable "private_subnet_cidr" {
-  type        = string
-}
-
-variable "public_subnet_az" {
-  type        = string
-}
-
-variable "private_subnet_az" {
-  type        = string
-}
-
-variable "ami_id" {
-  type = string
-}
-
-variable "instance_type" {
-  type = string
-}
-
-variable "key_name" {
-  type = string
-}
-
-variable "bucket_name" {
-  type = string
-}
-
-variable "db_name" {
-  type = string
+variable "region" {
+  default = "ap-south-1"
 }
 
 variable "db_username" {
-  type = string
+  default = "intel"
 }
 
 variable "db_password" {
-  type      = string
-  sensitive = true
+  default = "intel123"
 }
