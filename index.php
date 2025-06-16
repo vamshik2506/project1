1426596dab4dc897927b6302b6697c29fa8c0a25
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-16 14:09:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,20 +6,18 @@
   Email: <input type="email" name="email"><br>
   <input type="submit">
 </form>
-
 <?php
-$servername = "<REPLACE_WITH_RDS_ENDPOINT>";
+$servername = "${db_host}";
 $username = "intel";
 $password = "intel123";
 $dbname = "intel";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 if(isset($_POST['firstname']) && isset($_POST['email'])) {
   $firstname = $conn->real_escape_string($_POST['firstname']);
   $email = $conn->real_escape_string($_POST['email']);
-
   $sql = "INSERT INTO data (firstname, email) VALUES ('$firstname', '$email')";
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
