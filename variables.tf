79d7b5bff2676d40800f6b96f39b292f00280d35
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-06-17 11:37:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,80 @@
-variable "region" {
-  default = "ap-south-1"
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr_a" {
+  description = "CIDR block for public subnet A"
+  type        = string
+}
+
+variable "public_subnet_cidr_b" {
+  description = "CIDR block for public subnet B"
+  type        = string
+}
+
+variable "private_subnet_cidr_a" {
+  description = "CIDR block for private subnet A"
+  type        = string
+}
+
+variable "private_subnet_cidr_b" {
+  description = "CIDR block for private subnet B"
+  type        = string
+}
+
+variable "public_subnet_az_a" {
+  description = "Availability Zone for public subnet A"
+  type        = string
+}
+
+variable "public_subnet_az_b" {
+  description = "Availability Zone for public subnet B"
+  type        = string
+}
+
+variable "private_subnet_az_a" {
+  description = "Availability Zone for private subnet A"
+  type        = string
+}
+
+variable "private_subnet_az_b" {
+  description = "Availability Zone for private subnet B"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name for EC2 access"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
 }
 
 variable "db_username" {
-  default = "intel"
+  description = "Database username"
+  type        = string
 }
 
 variable "db_password" {
-  default = "intel123"
+  description = "Database password"
+  type        = string
 }
